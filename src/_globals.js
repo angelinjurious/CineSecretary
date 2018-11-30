548588535b11397b5634a2198aef7401d1c272e5
ben sturley <bensturley@gmail.com>
2018-11-30 08:36:43 +0000

    First commit

---
+++ src/_globals.js
@@ -0,0 +1,12 @@
+// 
+// _globals.js
+// 
+const globals = function() {
+
+    return {
+        APP_NAME:               'cinesecretary',
+        APP_NAME_FRIENDLY:      'CineSecretary'
+    };
+};
+
+module.exports = globals();
---
+++ src/app.js
@@ -0,0 +1,6 @@
+// 
+// app.js
+// 
+const globals = require('./_globals');
+console.log(globals.APP_NAME);
+console.log(globals.APP_NAME_FRIENDLY);
